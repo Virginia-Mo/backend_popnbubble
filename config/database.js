db46d4e48a827ec4d74e4dbe1804054017fe162a
Virginia-Mo <antonio.vir@hotmail.com>
2022-10-11 09:54:33 +0200

    commit

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'ex'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'popnbubble'),
-      user: env('DATABASE_USERNAME', 'popadmin'),
-      password: env('DATABASE_PASSWORD', 'popnbubblepassword'),
+      database: env('DATABASE_NAME', 'ex'),
+      user: env('DATABASE_USERNAME', 'ex'),
+      password: env('DATABASE_PASSWORD', 'ex'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
