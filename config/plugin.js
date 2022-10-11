9677c53c34a6bb0c06cb13258d755adc6490bf53
Virginia-Mo <antonio.vir@hotmail.com>
2022-10-11 23:39:13 +0200

    plugn

--- config/plugin.js
+++ config/plugin.js
@@ -6,5 +6,10 @@ module.exports = ({ env }) => ({
       api_key: env('CLOUDINARY_KEY'),
       api_secret: env('CLOUDINARY_SECRET'),
     },
+    actionOptions: {
+      upload: {},
+      uploadStream: {},
+      delete: {},
+    },
   },
 });
