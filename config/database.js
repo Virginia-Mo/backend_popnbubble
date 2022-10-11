f6020423de3a8e976cabc578c572a645b80f5336
Virginia-Mo <antonio.vir@hotmail.com>
2022-10-11 11:08:26 +0200

    test admin

--- config/database.js
+++ config/database.js
@@ -1,13 +1,11 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'popnbubble'),
-      user: env('DATABASE_USERNAME', 'popnbubble'),
-      password: env('DATABASE_PASSWORD', 'mypopnbubblepassword'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
+    host: env('DATABASE_HOST', '127.0.0.1'),
+    port: env.int('DATABASE_PORT', 5432),
+    database: env('DATABASE_NAME', 'popnbubble'),
+    user: env('DATABASE_USERNAME', 'popadmin'),
+    password: env('DATABASE_PASSWORD', 'popnbubblepassword'),
+    ssl: env.bool('DATABASE_SSL', false),
   },
 });
