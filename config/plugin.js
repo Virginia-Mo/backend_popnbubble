cf06d111868544558c47bd290d378a29e6913eaa
Virginia-Mo <antonio.vir@hotmail.com>
2022-10-11 23:57:05 +0200

    test cloudinary

--- config/plugin.js
+++ config/plugin.js
@@ -1,13 +1,19 @@
 module.exports = ({ env }) => ({
+  // ...
   upload: {
-      config:{
+    config: {
       provider: 'cloudinary',
       providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
   },
-},
+  // ...
 });
